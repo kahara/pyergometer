901320283f889cf30806d57c4201c707ed9fc7d0
Joni Kähärä <joni.kahara@gmail.com>
2012-04-07 12:34:56 +0300

    add basic session file parsing; construct session steps

--- pyergometer.py
+++ pyergometer.py
@@ -11,31 +11,32 @@ import logging
 log = logging.getLogger(__name__)
 
 
-import time
+import time, argparse
 from kettler import Kettler
+from session import Session
+#from simulator import Simulator
 
         
 if __name__ == '__main__':
-    from optparse import OptionParser
-    parser = OptionParser(description=__doc__, version="%%prog v%s" % __version__)
-    parser.add_option('-d', '--device', action="store", dest="device",
-        default='', help="Serial device.")
-    parser.add_option('-v', '--verbose', action="count", dest="verbose",
-        default=2, help="Increase the verbosity. Can be used twice for extra effect.")
-    parser.add_option('-q', '--quiet', action="count", dest="quiet",
-        default=0, help="Decrease the verbosity. Can be used twice for extra effect.")    
     
-    opts, args  = parser.parse_args()
+    parser = argparse.ArgumentParser(description=__doc__, version="v%s" % __version__)
     
-    bike = Kettler(device=opts.device)
+    parser.add_argument('-d', '--device', action='store', dest='device', default=None, help='Serial device')
+    parser.add_argument('-s', '--session', action='store', dest='session', default=None, help='Use this session file (jErgometer-like XML format)')
+    parser.add_argument('-l', '--log', action='store', dest='log', default=None, help='Record session stats to this log file')
+    parser.add_argument('-x', '--simulate', action='store', dest='simulate', default=0.5, type=float, help='Run session file against a simulated human with pulse reaction factor SIMULATE (0.0...1.0)')
     
-    for power in range(25, 100, 5):
-        print 'req power %d' % (power, )
-        bike.power(power)
-        
-        for i in range(0, 10):
-            bike.status()
-            
-            print '%d bpm\t%d rpm\t%d/%d W' % (bike.pulse, bike.rpm, bike.set_power, bike.actual_power)
-            
-            time.sleep(1)
+    args  = parser.parse_args()
+    
+    if args.simulate:
+        session = Session(session=args.session)
+        #simulator = Simulator(factor=args.factor)
+    
+    #bike = Kettler(device=args.device)    
+    # for power in range(25, 100, 5):
+    #     print 'req power %d' % (power, )
+    #     bike.power(power)   
+    #     for i in range(0, 10):
+    #         bike.status()
+    #         print '%d bpm\t%d rpm\t%d/%d W' % (bike.pulse, bike.rpm, bike.set_power, bike.actual_power)
+    #         time.sleep(1)
---
+++ session.py
@@ -0,0 +1,61 @@
+from lxml import etree
+import re
+
+
+class Session:
+    def __init__(self, session=None):
+        root = etree.parse(session).getroot()
+        self.name = root.attrib['name']
+        self.duration = self.parse_timestring(root.attrib['duration'])
+        
+        for timeevent in root.find('timeEvents'):
+
+            action = timeevent.find('action').attrib['type']
+            value = timeevent.find('action').attrib['value']
+
+            time = timeevent.attrib['time']
+            if time[0] == '+':
+                for t in range(0, self.parse_timestring(time.split('+')[1])-1):
+                    prevstep = self.steps[-1:][0]
+                    self.steps.append({
+                            'time': prevstep['time']+1,
+                            'power': prevstep['power'],
+                            'pulse': prevstep['pulse']
+                            })
+
+                prevstep = self.steps[-1:][0]
+                self.steps.append({
+                        'time': prevstep['time']+1,
+                        'power': value if action == 'power' else 0,
+                        'pulse': value if action == 'pulse' else 0
+                        })
+            
+            elif time == '0' and action == 'power':
+                self.steps = []
+                self.steps.append({
+                        'time': 0,
+                        'power': value,
+                        'pulse': 0
+                        })
+        
+        for i in range(0, self.duration - len(self.steps)):
+            prevstep = self.steps[-1]
+            self.steps.append({
+                    'time': prevstep['time']+1,
+                    'power': prevstep['power'],
+                    'pulse': prevstep['pulse']
+                    })
+            
+        self.logsteps = []
+        
+    def parse_timestring(self, s):
+        duration = 0
+        for component in re.findall('(\d*(h|m|s))', s):
+            if component[1] == 'h':
+                duration += int(component[0].split('h')[0]) * 3600
+            if component[1] == 'm':
+                duration += int(component[0].split('m')[0]) * 60
+            if component[1] == 's':
+                duration += int(component[0].split('s')[0])
+        
+        return duration
