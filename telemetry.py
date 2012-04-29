56e7dd702d19eb4a73783167d12eb0cdfa3b940a
Joni Kähärä <joni.kahara@gmail.com>
2012-04-29 21:49:24 +0300

    build telemetry on S3 instead of WebSockets

---
+++ telemetry.py
@@ -0,0 +1,34 @@
+import threading
+from time import sleep
+import boto
+from boto.s3.bucket import Bucket
+from boto.s3.key import Key
+from boto.cloudfront import CloudFrontConnection
+import json, datetime
+
+
+class Telemetry(threading.Thread):
+    def __init__(self, ergometer=None, session=None):
+        threading.Thread.__init__(self)
+        self.ergometer = ergometer
+        self.session = session
+        
+    def run(self):
+        connection = boto.connect_s3()
+        bucket = Bucket(connection, 'ergometria.async.fi')
+        k = Key(bucket)
+        k.key = 'live/latest.json'
+        
+        while True:
+            data = {
+                'timestamp': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S+0000'),
+                'pulse': self.ergometer.device.pulse,
+                'target_pulse': self.session.step(self.ergometer.pc)['pulse'],
+                'power': self.ergometer.device.power,
+                'rpm': self.ergometer.device.rpm,
+                }
+            f = open('www/' + k.key, 'w')
+            f.write(json.dumps(data))
+            f.close()
+            k.set_contents_from_filename('www/' + k.key, headers={'Cache-Control': 'max-age=0'})
+            sleep(1)
---
+++ www/live/script.js
@@ -0,0 +1,67 @@
+var g;
+var session = [];
+$(document).ready(function() {
+    
+    var w1 = $(window).width();
+    var h1 = $(window).height();
+    
+    var w2 = $('#readings').width();
+    
+    $('#graph').width(w1-w2-100);
+    $('#graph').height(h1-100);
+    
+    var live = false;
+    setInterval(function() {
+	$.ajax('latest.json', {
+	    cache: false,
+	    ifModified: true,
+	    dataType: 'json',
+	    timeout: 350,
+	    success: function(data) {
+		
+		if(!data) return;
+		
+		if(data['pulse'] == 0 && data['target_pulse'] == 0 && data['power'] == 0 && data['rpm'] == 0) return;
+		
+		if(!live) {
+		    live = true;
+		    $('.standby').hide().removeClass('blink');
+		    $('.live').show().addClass('blink');
+		    $('timestamp').show();
+		 
+		    session += data['timestamp'] + ',' + data['pulse'] + ',' + data['target_pulse'] + ',' + data['power'] + ',' + data['rpm'] + '\n';
+		    //session += (new Date(data['timestamp'])).getTime() + ',' + data['pulse'] + ',' + data['target_pulse'] + ',' + data['power'] + ',' + data['rpm'] + '\n';
+		    g = new Dygraph($('#graph').get(0), session, {
+			labels: ['Time', 'Pulse', 'Target pulse', 'Power', 'Rpm'],
+			colors: ['#dd0000', '#0000dd', '#00dd00', '#dddd00'],
+			//valueRange: [0, 200],
+			displayAnnotations: true,
+			labelsSeparateLines: true,
+			drawPoints: true,
+			hideOverlayOnMouseOut: true
+			//legend: 'always'
+		    });
+		}
+		console.log(data);
+		_.each(['timestamp', 'pulse', 'target_pulse', 'power', 'rpm'], function(stat) {
+		    $('.' + stat).text(data[stat]);
+		});
+		
+		console.log(session);
+		
+		session += data['timestamp'] + ',' + data['pulse'] + ',' + data['target_pulse'] + ',' + data['power'] + ',' + data['rpm'] + '\n';
+		//session += (new Date(data['timestamp'])).getTime() + ',' + data['pulse'] + ',' + data['target_pulse'] + ',' + data['power'] + ',' + data['rpm'] + '\n';
+		g.updateOptions({ 'file': session });
+	    }
+	});
+    }, 500);
+    
+    setInterval(function() {
+	$('.blink').addClass('shadow');
+	$('.blink').fadeTo(0, 1.00, function() {
+	    $(this).fadeTo(500, 0.85, function() {
+		$('.blink').removeClass('shadow');
+	    });
+	});
+    }, 1500);
+});
