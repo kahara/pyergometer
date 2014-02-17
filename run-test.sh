03de384bf67c5add5b1484e4625a3bccbbb4f362
Joni Kähärä <joni.kahara@async.fi>
2014-02-17 21:06:16 +0200

    move pyergometer from MacBook to Raspberry Pi

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 FILENAME=`date +'%Y-%m-%dT%H:%M:%S'`
-./pyergometer.py --session session-test.xml --device /dev/tty.usbserial-FTUKKECU --log ./data-test/$FILENAME.json
+./pyergometer.py --session session-test.xml --device /dev/ttyUSB0 --log ./data-test/$FILENAME.json
 #./dumplog.py ./data/$FILENAME.json >./data/$FILENAME.csv
 #./upload.py
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 FILENAME=`date +'%Y-%m-%dT%H:%M:%S'`
-./pyergometer.py --session session.xml --device /dev/tty.usbserial-FTUKKECU --log ./data/$FILENAME.json
+./pyergometer.py --session session.xml --device /dev/ttyUSB0 --log ./data/$FILENAME.json
 ./dumplog.py ./data/$FILENAME.json >./data/$FILENAME.csv
 ./upload.py
