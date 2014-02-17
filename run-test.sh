434b3cc1ca112cf7de66a1090fd6845a71a43d96
Joni Kähärä <joni.kahara@async.fi>
2014-02-17 21:19:52 +0200

    "testing" tweaks

--- run-test.sh
+++ run-test.sh
@@ -2,5 +2,5 @@
 
 FILENAME=`date +'%Y-%m-%dT%H:%M:%S'`
 ./pyergometer.py --session session-test.xml --device /dev/ttyUSB0 --log ./data-test/$FILENAME.json
-#./dumplog.py ./data/$FILENAME.json >./data/$FILENAME.csv
+./dumplog.py ./data-test/$FILENAME.json >./data-test/$FILENAME.csv
 #./upload.py
